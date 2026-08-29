6f162e85ca59753137f0c583f6819c794cbff401
ShrideepP <shrideeppakhare2005@gmail.com>
2026-08-30 00:29:10 +0530

    chore: setup Tailwind CSS, daisyUI, and Prettier

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,12 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  srcDir: './src/web/',
+  srcDir: "./src/web/",
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
